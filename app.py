f2b99a9195b0bd3485033f5f991c768712cf336d
Gabriel Martin del Campo <gmartindelcye@gmail.com>
2022-04-30 00:25:26 -0500

    uso de relaciones (código no funciona) err lin 83

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from typing import Optional
+from typing import List, Optional 
 
-from sqlmodel import Field, Session, SQLModel, create_engine, or_, select
+from sqlmodel import Field, Relationship, Session, SQLModel, create_engine, or_, select
 
 
 class Team(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(index=True)
     headquarters: str
+
+    heroes: List["Hero"] = Relationship(back_populates="team")
 
 
 class Hero(SQLModel, table=True):
@@ -16,6 +18,7 @@ class Hero(SQLModel, table=True):
     age: Optional[int] = Field(default=None, index=True)
 
     team_id: Optional[int] = Field(default=None, foreign_key="team.id")
+    team: Optional[Team] = Relationship(back_populates="heroes")
 
 
 sqlite_file_name = "database.db"
@@ -32,42 +35,62 @@ def create_heroes():
     with Session(engine) as session:
         team_preventers = Team(name="Preventers", headquarters="Sharp Tower")
         team_z_force = Team(name="Z-Force", headquarters="Sister Margaret’s Bar")
+
+        hero_deadpond = Hero(
+            name="Deadpond", secret_name="Dive Wilson", team=team_z_force
+        )
+        hero_rusty_man = Hero(
+            name="Rusty-Man", secret_name="Tommy Sharp", age=48, team=team_preventers
+        )
+        hero_spider_boy = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
+        session.add(hero_deadpond)
+        session.add(hero_rusty_man)
+        session.add(hero_spider_boy)
+        session.commit()
+
+        session.refresh(hero_deadpond)
+        session.refresh(hero_rusty_man)
+        session.refresh(hero_spider_boy)
+
+        print("Created hero:", hero_deadpond)
+        print("Created hero:", hero_rusty_man)
+        print("Created hero:", hero_spider_boy)
+
+        hero_spider_boy.team = team_preventers
+        session.add(hero_spider_boy)
+        session.commit()
+        session.refresh(hero_spider_boy)
+        print("Updated hero:", hero_spider_boy)
+
+        hero_black_lion = Hero(name="Black Lion", secret_name="Trevor Challa", age=35)
+        hero_sure_e = Hero(name="Princess Sure-E", secret_name="Sure-E")
+        team_wakaland = Team(
+            name="Wakaland",
+            headquarters="Wakaland Capital City",
+            heroes=[hero_black_lion, hero_sure_e],
+        )
+        session.add(team_wakaland)
+        session.commit()
+        session.refresh(team_wakaland)
+        print("Team Wakaland:", team_wakaland)
+
+        hero_tarantula = Hero(name="Tarantula", secret_name="Natalia Roman-on", age=32)
+        hero_dr_weird = Hero(name="Dr. Weird", secret_name="Steve Weird", age=36)
+        hero_cap = Hero(
+            name="Captain North America", secret_name="Esteban Rogelios", age=93
+        )
+
+        team_preventers.heroes.append(hero_tarantula)
+        team_preventers.heroes.append(hero_dr_weird)
+        team_preventers.heroes.append(hero_cap)
         session.add(team_preventers)
-        session.add(team_z_force)
         session.commit()
-
-        hero_1 = Hero(
-            name="Deadpond", secret_name="Dive Wilson", team_id=team_z_force.id
-        )
-        hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
-        hero_3 = Hero(
-            name="Rusty-Man",
-            secret_name="Tommy Sharp",
-            age=48,
-            team_id=team_preventers.id,
-        )
-        hero_4 = Hero(name="Tarantula", secret_name="Natalia Roman-on", age=32)
-        hero_5 = Hero(name="Black Lion", secret_name="Trevor Challa", age=35)
-        hero_6 = Hero(name="Dr. Weird", secret_name="Steve Weird", age=36)
-        hero_7 = Hero(name="Captain North", secret_name="Esteban Roger", age=93)
-
-        session.add(hero_1)
-        session.add(hero_2)
-        session.add(hero_3)
-        session.add(hero_4)
-        session.add(hero_5)
-        session.add(hero_6)
-        session.add(hero_7)
-
-        session.commit()
-
-        session.refresh(hero_1)
-        session.refresh(hero_2)
-        session.refresh(hero_3)
-        session.refresh(hero_4)
-        session.refresh(hero_5)
-        session.refresh(hero_6)
-        session.refresh(hero_7)
+        session.refresh(hero_tarantula)
+        session.refresh(hero_dr_weird)
+        session.refresh(hero_cap)
+        print("Preventers new hero:", hero_tarantula)
+        print("Preventers new hero:", hero_dr_weird)
+        print("Preventers new hero:", hero_cap)
 
 
 def select_heroes():
@@ -177,17 +200,17 @@ def remove_hero_from_team(id: int):
 def main():
     create_db_and_tables()
     create_heroes()
-    select_heroes()
+    # select_heroes()
     # select_hero_by_name("Deadpond")
     # select_hero_by_id(6)
     # select_heroes_by_age_range(35, 45)
     # select_heroes_by_out_age_range(35, 90)
     # update_hero_age(id=2, age=16)
     # delete_hero(5)
-    select_heroes_teams()
-    assign_hero_to_team(2, 7)
-    remove_hero_from_team(1)
-    select_heroes_and_teams()
+    # select_heroes_teams()
+    # assign_hero_to_team(2, 7)
+    # remove_hero_from_team(1)
+    # select_heroes_and_teams()
 
 
 if __name__ == "__main__":
